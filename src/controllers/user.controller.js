3fcbc95bfc584b64fbd6ae0caf1b3438472d922d
Kunal Mandave <mandavekunal@gmail.com>
2024-02-13 14:51:55 +0530

    user controller added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,154 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { response } from "express";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const newRefreshToken = user.generateRefreshToken();
+
+    user.refreshToken = newRefreshToken;
+    await user.save({
+      validateBeforeSave: false,
+    });
+
+    return { accessToken, newRefreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access tokens."
+    );
+  }
+};
+const registerInformant = asyncHandler(async (req, res) => {
+  const { name, username, phoneNumber, email, password } = req.body;
+
+  if (
+    [name, username, phoneNumber, email, password].some((field) => {
+      field?.trim() === "" || field.trim() === undefined;
+    })
+  ) {
+    throw new ApiError(400, "All fields must be required");
+  }
+
+  let avatarLocalPath = req.files?.avatar[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const userExist = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (userExist) {
+    throw new ApiError(409, "User with username or email already exists");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(500, "Something went wrong uploading avatar");
+  }
+
+  const user = await User.create({
+    name,
+    username,
+    phoneNumber,
+    email,
+    password,
+    avatar: avatar.url,
+  });
+
+  const userCreated = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userCreated) {
+    throw new ApiError(500, "Something went wrong while registering user.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200), user, "User registered successfully");
+});
+
+const login = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!email && !username) {
+    throw new ApiError(400, "Username or email is required.");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Passwords do not match");
+  }
+
+  const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken: newRefreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, // This will remove the field from the document
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+// this is for users all created query
+const getAllAnimalQuery = asyncHandler(async (req, res) => {});
